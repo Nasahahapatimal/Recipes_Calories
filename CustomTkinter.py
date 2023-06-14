e643a03418cdc51a732f574a39d9ec6af9012e35
Nasahahapatimal <dusan.blagojevic1990@gmail.com>
2023-06-14 19:34:11 +0200

    Added calories button and dodaj_namirnicu

--- CustomTkinter.py
+++ CustomTkinter.py
@@ -173,20 +173,72 @@ def namirnice():
     frame3 = customtkinter.CTkFrame(t2,fg_color = "transparent",width=200,height=200)
     frame3.place(relx = 0,rely=0.5)
 
-    b_t2 = customtkinter.CTkButton(frame2,text = "Obrisi namirnicu")
+    b_t2 = customtkinter.CTkButton(frame2,text = "Obrisi namirnicu",command=lambda:delete_selected_namirnica())
     b_t2.pack(padx=5, pady=5)
 
-    b_t2 = customtkinter.CTkButton(frame2,text = "Dodaj namirnicu")
+    b_t2 = customtkinter.CTkButton(frame2,text = "Dodaj namirnicu",command=lambda:dodaj_namirnicu())
     b_t2.pack(padx=5, pady=5)
 
-    b1_t2 = customtkinter.CTkButton(frame2,text = "Kalorije - namirnica")
+    b1_t2 = customtkinter.CTkButton(frame2,text = "Kalorije - namirnica",command=lambda:l_t2.configure(text = rec.ocitaj_kalorije_namirnica(select_value_namirnica())))
     b1_t2.pack(padx=5, pady=5)
 
-    b2_t2 = customtkinter.CTkButton(frame2,text = "Kalorije - sve namirnice")
+    b2_t2 = customtkinter.CTkButton(frame2,text = "Kalorije - sve namirnice",command=lambda:l_t2.configure(text = rec.ocitaj_kalorije_sve_namirnice()))
     b2_t2.pack(padx=5, pady=5)
 
     l_t2 = customtkinter.CTkLabel(frame3,text = "")
     l_t2.pack(padx=5, pady=5)
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------#
+    def delete_selected_namirnica():
+        selected_indices = listbox_nam.curselection()
+        if selected_indices:
+            selected_recipe = selected_indices[0]  # Assuming only one item is selected
+            selected_value = listbox_nam.get(selected_recipe)
+            print(selected_value)
+            rec.obrisi_namirnicu(selected_value)
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------#
+    def dodaj_namirnicu():
+        t3 = customtkinter.CTkToplevel(root)
+        t3.geometry("800x200")
+ 
+        frame1 = customtkinter.CTkFrame(t3,fg_color = "transparent")
+        frame1.pack(side="top", padx=5, pady=5)
+
+        b = customtkinter.CTkButton(frame1, text="Dodaj namirnicu",command=lambda:rec.dodaj_namirnicu(e_t3.get(),combobox_unit.get(),combobox_amount.get(),e1_t3.get()))
+        b.grid(row=2, column=0, padx=3, pady=3, sticky="ew")
+
+        l1_t3 = customtkinter.CTkLabel(frame1, text="Naziv Namirnice")
+        l1_t3.grid(row=0, column=0, padx=3, pady=3, sticky="ew")
+
+        e_t3 = customtkinter.CTkEntry(frame1,  width=150)
+        e_t3.grid(row=1, column=0 , padx=3, pady=3, sticky="ew")
+
+        l2_t3 = customtkinter.CTkLabel(frame1, text="Jedinica mere")
+        l2_t3.grid(row=0, column=1, padx=3, pady=3, sticky="ew")
+
+        combobox_unit = customtkinter.CTkComboBox(frame1, values=list(set(rec.namirnice_df["jedinicamere"].to_list())), variable="", width=150)
+        combobox_unit.grid(row=1, column=1, padx=3, pady=3, sticky="ew")
+
+        l3_t3 = customtkinter.CTkLabel(frame1, text="Kolicina")
+        l3_t3.grid(row=0, column=2, padx=3, pady=3, sticky="ew")
+
+        combobox_amount = customtkinter.CTkComboBox(frame1, values=[str(value) for value in set(rec.namirnice_df["kolicina"].to_list())], variable="", width=150)
+        combobox_amount.grid(row=1, column=2, padx=3, pady=3, sticky="ew")
+
+        l3_t3 = customtkinter.CTkLabel(frame1, text="Kalorije",width=150)
+        l3_t3.grid(row=0, column=3, padx=3, pady=3, sticky="ew")
+
+        e1_t3 = customtkinter.CTkEntry(frame1,  width=150)
+        e1_t3.grid(row=1, column=3, padx=3, pady=3, sticky="ew")
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------#
+    def select_value_namirnica():
+        selected_indices = listbox_nam.curselection()
+        if selected_indices:
+            selected_ingredient = selected_indices[0]  # Assuming only one item is selected
+            selected_value = listbox_nam.get(selected_ingredient)
+            return selected_value
+
 
 
 
